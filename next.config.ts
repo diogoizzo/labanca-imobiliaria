d637824b975d55308e30e53fed65e234b9c067b8
diogoizzo <dizzodev@gmail.com>
2025-10-04 17:22:17 -0300

    ajustes tsconfig e nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,23 @@
 import type { NextConfig } from "next";
+import path from "path";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...(config.resolve.alias ?? {}),
+            "@": path.resolve(__dirname, "src"), // <- força @ -> src
+        };
+        config.resolve.extensions = [
+            ".ts",
+            ".tsx",
+            ".js",
+            ".jsx",
+            ".mjs",
+            ".cjs",
+            ".json",
+        ];
+        return config;
+    },
+};
 
 export default nextConfig;
